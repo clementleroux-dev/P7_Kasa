5a8c74ba312e140c3000792283aa796083884ef0
clementleroux-dev <clement.lerouxvi2@gmail.com>
2022-10-31 15:50:27 +0100

    Initializing project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="lodgings-container">
+      <div className="lodge-container">
+        {/*<img />*/}
+        <div className="lodge-title">
+          <h2>Titre de la location</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
